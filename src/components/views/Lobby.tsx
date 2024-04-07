380908b8cf6e4f67f4dda97c2116dcb310587611
Grizzlytron <lucas.baer@hispeed.ch>
2024-04-07 11:27:08 +0200

    Implemented design adaptions and added Quit button. Added QuitPopup functionality. #28 #27 #26 #11 #122

--- src/components/views/Lobby.tsx
+++ src/components/views/Lobby.tsx
@@ -1,9 +1,11 @@
-import React, {useState} from "react";
+import React, {createContext, useState} from "react";
 import { Button } from "components/ui/Button";
+import QuitPopup from "components/popup-ui/QuitPopup";
 import "styles/views/Lobby.scss";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import {Gamemode, Player} from "../../types";
+import {useNavigate} from "react-router-dom";
 
 const GamemodeItem = ({
     gamemode,
@@ -40,11 +42,26 @@ const players= [
     { name: "The big rock", icon: "/images/PinkBunny.jpg", token: "3"},
 ];
 
-let lobbyOwner: Boolean;
-lobbyOwner = true;
+let lobbyOwner = true;
+
+let lobby;
+// method to check whether a player is the lobby Owner, also can be shown in the game lobby screen when implemented.
+function isLobbyOwner(player: Player) {
+    return player === lobby.owner;
+}
+
+// helper method to check for the winner of last round
+function islastWinner(player: Player)  {
+    return player === lobby.lastWinner;
+}
+
+export const context = createContext();
 
 const LobbyPage = () => {
-    const [selectedGamemode, setSelectedGamemode] = useState<Gamemode >(null);
+    const navigate = useNavigate();
+    const [selectedGamemode, setSelectedGamemode] = useState<Gamemode>(null);
+    const [quitPopup, setQuitPopup] = useState(false);
+
 
     const selectGamemode = (gamemode: Gamemode) => {
         setSelectedGamemode((prevSelectedGamemode) =>
@@ -52,12 +69,17 @@ const LobbyPage = () => {
         );
     };
 
+    const handleQuit = () => {
+        setQuitPopup((prevState) => !prevState);
+    }
+
     const startGame = () => {
         alert("To be implemented!");
     };
 
     let content = (
         <div>
+            <p>Select a Gamemode</p>
             <ul className="gamemode list">
                 {gamemodes.map((gamemode: Gamemode) => (
                     <li key={gamemode.gamemodeName}>
@@ -69,52 +91,75 @@ const LobbyPage = () => {
                     </li>
                 ))}
             </ul>
-            <div className="gamemode button-container">
-                <Button
-                    width="100%"
-                    onClick={() => startGame()}
-                    disabled={!selectedGamemode || !lobbyOwner}
-                >
-                    Start Game
-                </Button>
-            </div>
+
+
         </div>
     );
 
     let usercontent = (
         <div>
+            <p> Currently active players </p>
             <ul className="player list">
                 {players.map((player: Player) => (
                     <li key={player.token}>
                         <div className="player container">
                             <div className="player icon">
                                 <img src={player.icon} alt="player icon"/>
-
                             </div>
-                            {player.name}
+                            <div className="player name">{player.name}
+                            </div>
                         </div>
                     </li>
                 ))}
             </ul>
+
+
         </div>
+
     );
 
     return (
+
         <div className="container-wrapper">
             <BaseContainer>
                 <div className="lobbypage container">
                     <h2>Grealish Lobby</h2>
-                    <p className="lobby p">Select a Gamemode</p>
-                    {content}
-                </div>
-                <div className="lobbypage container">
-                    <h2>List of players</h2>
-                    {usercontent}
+                    <div className="lobbypage game-and-players-container">
+                        <div className="gamemode standard">
+                            {content}
+                        </div>
+                        <BaseContainer className="player-list-container">
+                            <div>
+                                {usercontent}
+                            </div>
+                        </BaseContainer>
+
+                    </div>
+                    <div className="button-container">
+                        <Button
+                            className="button"
+                            onClick={() => startGame()}
+                            disabled={!selectedGamemode || !lobbyOwner}
+                        >
+                            Start Game
+                        </Button>
+                        <Button
+                            className="button"
+                            onClick={() => handleQuit()}
+                        >
+                            Quit
+                        </Button>
+                    </div>
                 </div>
             </BaseContainer>
+            {quitPopup && (
+                <context.Provider value={{ quitPopup, setQuitPopup }}>
+                    <QuitPopup />
+                </context.Provider>)
+            }
         </div>
-    );
 
-};
+    );
+}
 
 export default LobbyPage;
