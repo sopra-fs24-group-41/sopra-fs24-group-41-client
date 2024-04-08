b9e6b97ef67c4e57ea1430bcacd49c731ea9680d
na50r <shanrosan@gmail.com>
2024-04-08 07:17:01 +0200

    Added back profile page
    Fixed up indentation

--- src/components/popup-ui/ImageDropdown.tsx
+++ src/components/popup-ui/ImageDropdown.tsx
@@ -2,18 +2,17 @@ import PropTypes from "prop-types";
 import "../../styles/popup-ui/ImageDropdown.scss";
 import React from "react";
 
-
 const ImageDropdown = ({ images, onSelectImage }) => {
   return (
-      <div className="image-dropdown-menu">
-        {images.map((image, index) => (
-            <img
-                key={index}
-                src={`/images/${image}`}
-                onClick={() => onSelectImage(image)}
-            />
-        ))}
-      </div>
+    <div className="image-dropdown-menu">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={`/images/${image}`}
+          onClick={() => onSelectImage(image)}
+        />
+      ))}
+    </div>
   );
 };
 
@@ -22,4 +21,4 @@ ImageDropdown.propTypes = {
   onSelectImage: PropTypes.func.isRequired,
 };
 
-export default ImageDropdown
+export default ImageDropdown;
