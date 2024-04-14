1de569b9bfadb96ee1f47ccf90282e93be83cb69
Alexandru Hurjui (Aquamarine-12) <alexandru-mihai.hurjui@uzh.ch>
2024-04-14 19:46:10 +0200

    Create WordBoard #31

--- src/components/views/GameBoard/WordBoard.tsx
+++ src/components/views/GameBoard/WordBoard.tsx
@@ -8,8 +8,52 @@ import PropTypes from "prop-types";
 import "styles/views/Game.scss";
 import { User } from "types";
 
+const Word = ({ key, content }) => {
+  return <div className="word">{content}</div>;
+};
+
+Word.propTypes = {
+  key: PropTypes.string,
+  content: PropTypes.string,
+};
+
+const WordRow = ({ key, words }) => {
+  return <div className="wordrow"> {words} </div>;
+};
+
+WordRow.propTypes = {
+  key: PropTypes.string,
+  words: PropTypes.node,
+};
+
 const WordBoard = () => {
-  return <BaseContainer className="wordboard container">text</BaseContainer>;
+  const [wordList, setWordList] = useState<String>([]);
+
+  const addWord = (word: string) => {
+    setWordList([...wordList, word]);
+  };
+
+  const createWordMatrix = () => {
+    let result = [];
+    let wordRow = [];
+    for (let i = 0; i < wordList.length; i++) {
+      if (i > 0 && i % 8 === 0) {
+        result.push(<WordRow key={wordRow.toString()} words={wordRow}></WordRow>);
+        wordRow = [];
+      }
+      wordRow.push(<Word key={wordList[i]} content={wordList[i]}></Word>);
+    }
+    result.push(<WordRow key={wordRow.toString()} words={wordRow}></WordRow>);
+
+    console.log(result);
+    return result;
+  };
+
+  return (
+    <BaseContainer className="wordboard container">
+      {createWordMatrix()}
+    </BaseContainer>
+  );
 };
 
 export default WordBoard;
