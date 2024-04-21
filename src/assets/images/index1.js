44c5b4399d6b16a0cd30e18cdbe8c82006e93298
na50r <shanrosan@gmail.com>
2024-04-21 15:25:25 +0200

    Added some profile pictures

--- src/assets/images/index1.js
+++ src/assets/images/index1.js
@@ -1,11 +1,19 @@
 import PinkBunny from "../../assets/images/PinkBunny.JPG"
 import RedSquid from "../../assets/images/RedSquid.JPG"
 import BlueFrog from "../../assets/images/BlueFrog.JPG"
+import PixelShiba from "../../assets/images/PixelShiba.jpg"
+import SunglassesGorilla from "../../assets/images/SunglassesGorilla.jpg"
+import AlienDude from "../../assets/images/AlienDude.jpg"
+import SpaceDude from "../../assets/images/SpaceDude.jpg"
 
 const IMAGES = {
     BlueFrog: BlueFrog,
     PinkBunny: PinkBunny,
     RedSquid : RedSquid,
+    PixelShiba : PixelShiba,
+    SunglassesGorilla : SunglassesGorilla,
+    AlienDude : AlienDude,
+    SpaceDude : SpaceDude,
 }
 
 export default IMAGES
