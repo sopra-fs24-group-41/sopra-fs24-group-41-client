34af6e441d1e2f05a7fae2802d7d16661a6324bd
JacquelineUlken <ulken.jacqueline@gmail.com>
2024-04-23 16:47:59 +0200

    Renamed to WordButton to avoid conflicts with Word

--- src/components/views/GameBoard/WordButton.tsx
+++ src/components/views/GameBoard/WordButton.tsx
@@ -1,15 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { api, handleError } from "helpers/api";
-import { Spinner } from "components/ui/Spinner";
 import { Button } from "components/ui/Button";
-import { useNavigate } from "react-router-dom";
-import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import "styles/views/Game.scss";
 import "styles/ui/Button.scss";
-import { User } from "types";
 
-const Word = (props) => {
+const WordButton = (props) => {
     return (
         <Button {...props} className="word">
             {props.children}
@@ -17,8 +12,8 @@ const Word = (props) => {
     );
 };
 
-Word.propTypes = {
+WordButton.propTypes = {
     children: PropTypes.node,
 };
 
-export default Word;
+export default WordButton;
