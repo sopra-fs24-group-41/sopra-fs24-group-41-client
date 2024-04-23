05b2df8c8bcaca9f076b7341ad2c8bea6e2deb90
JacquelineUlken <ulken.jacqueline@gmail.com>
2024-04-23 16:49:27 +0200

    Added api call to make actual word merge

--- src/components/views/GameBoard/Game.tsx
+++ src/components/views/GameBoard/Game.tsx
@@ -1,22 +1,54 @@
 import React, { useEffect, useState, createContext, useContext } from "react";
-import { api, handleError } from "helpers/api";
-import { Spinner } from "components/ui/Spinner";
-import { Button } from "components/ui/Button";
-import { useNavigate } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
-import PropTypes from "prop-types";
 import "styles/views/Game.scss";
-import { User } from "types";
 import WordBoard from "./WordBoard";
 import WordMergeBar from "./WordMergeBar";
+import Player from "models/Player";
+import { api, handleError } from "helpers/api";
+import Word from "models/Word";
+import WordButton from "./WordButton";
 
 export const nextWordIndexContext = createContext(123);
 
 export const mergeWordListContext = createContext([]);
 
+export const wordListContext = createContext([]);
+
+export const playerContext = createContext(new Player());
+const testPlayerToken = "f9303651-c5cb-4546-9345-d57de2e48ca3";
+const testPlayerId = 4;
+const testLobbyCode = 9399;
+
 const Game = () => {
     const [nextWordIndex, setNextWordIndex] = useState(0);
     const [mergeWordList, setMergeWordList] = useState<String>([]);
+    const [player, setPlayer] = useState<Player>(new Player);
+    const [wordList, setWordList] = useState<Word[]>([]);
+
+    useEffect(() => {
+        const fetchPlayer = async () => {
+            try {
+                const playerId = testPlayerId; // TODO: replace with actual ID
+                const playerToken = testPlayerToken; // TODO: replace with actual token
+                const lobbyCode = testLobbyCode; // TODO: replace with actual lobby code
+
+                let response = await api.get(`/lobbies/${lobbyCode}/players/${playerId}`, { headers: { "playerToken": playerToken } });
+                let foundPlayer = new Player(response.data);
+                foundPlayer.id = playerId;
+                foundPlayer.token = playerToken;
+                foundPlayer.lobbyCode = lobbyCode;
+                setPlayer(foundPlayer);
+            } catch (error) {
+                alert("Error: " + error.message);
+            }
+        };
+        fetchPlayer();
+    }, []);
+
+    useEffect(() => {
+        setWordList(player?.getWords());
+    }, [player]);
+
 
     return (
         <BaseContainer className="game container">
@@ -26,8 +58,16 @@ const Game = () => {
                 <mergeWordListContext.Provider
                     value={{ mergeWordList, setMergeWordList }}
                 >
-                    <WordMergeBar></WordMergeBar>
-                    <WordBoard></WordBoard>
+                    <wordListContext.Provider
+                        value={{ wordList, setWordList }}
+                    >
+                        <playerContext.Provider
+                            value={{ player, setPlayer }}
+                        >
+                            <WordMergeBar></WordMergeBar>
+                            <WordBoard></WordBoard>
+                        </playerContext.Provider>
+                    </wordListContext.Provider>
                 </mergeWordListContext.Provider>
             </nextWordIndexContext.Provider>
         </BaseContainer>
--- src/components/views/GameBoard/WordBoard.tsx
+++ src/components/views/GameBoard/WordBoard.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState, useContext } from "react";
-import { api, handleError } from "helpers/api";
-import { Spinner } from "components/ui/Spinner";
-import { Button } from "components/ui/Button";
-import { useNavigate } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import "styles/views/Game.scss";
-import { User } from "types";
-import Word from "./Word";
-import { nextWordIndexContext, mergeWordListContext } from "./Game";
+import Word from "models/Word";
+import WordButton from "./WordButton";
+import { api, handleError } from "helpers/api";
+import { nextWordIndexContext, mergeWordListContext, wordListContext, playerContext} from "./Game";
 
 const WordRow = ({ key, words }) => {
     return <div className="wordrow"> {words} </div>;
@@ -21,29 +18,38 @@ WordRow.propTypes = {
 
 const WordBoard = () => {
     const { nextWordIndex, setNextWordIndex } =
-        useContext(nextWordIndexContext);
+    useContext(nextWordIndexContext);
     const { mergeWordList, setMergeWordList } =
-        useContext(mergeWordListContext);
+    useContext(mergeWordListContext);
+    const { wordList, setWordList } =
+    useContext(wordListContext);
+    const { player, setPlayer} =
+    useContext(playerContext);
 
-    const [wordList, setWordList] = useState<String>([
-        "Water",
-        "Fire",
-        "Earth",
-        "Wind",
-    ]);
-
-    const addWordToWordBoard = (word: string) => {
+    const addWordToWordBoard = (name: string) => {
         let i;
         // See first if the given word is already discovered.
-        for (i = 0; i < wordList.length; i++) if (word === wordList[i]) break;
-        if (i === wordList.length) setWordList([...wordList, word]);
+        for (i = 0; i < wordList.length; i++) if (name === wordList[i].name) break;
+        if (i === wordList.length) setWordList([...wordList, new Word({name : name})]);
     };
 
-    const getMergeResult = (word1: string, word2: string) => {
-        return word1 + word2;
+    const play = async (word1: string, word2: string) => {
+        try {
+            let response = await api.put(`/lobbies/${player.lobbyCode}/players/${player.id}`,
+                [new Word({name : word1}), new Word({name : word2})],
+                { headers: { "playerToken": player.token } });
+            player.points = response.data.points;
+            player.playerWords = response.data.playerWords;
+            player.targetWord = response.data.targetWord;
+            
+            return response.data.resultWord.name;
+
+        } catch (error) {
+            alert("Error: " + error.message);
+        }
     };
 
-    const addWordToMerge = (word: string) => {
+    const addWordToMerge = async (word: string) => {
         let newWordIndex = nextWordIndex;
         let newWordList = mergeWordList;
 
@@ -56,9 +62,9 @@ const WordBoard = () => {
         newWordIndex++;
 
         if (newWordIndex === 2) {
-            newWordList[newWordIndex] = getMergeResult(
+            newWordList[newWordIndex] = await play(
                 newWordList[0],
-                newWordList[1]
+                newWordList[1],
             );
             addWordToWordBoard(newWordList[newWordIndex]);
         }
@@ -73,23 +79,23 @@ const WordBoard = () => {
         for (let i = 0; i < wordList.length; i++) {
             if (i > 0 && i % 8 === 0) {
                 result.push(
-                    <WordRow key={wordRow.toString()} words={wordRow}></WordRow>
+                    <WordRow key={wordRow.toString()} words={wordRow}></WordRow>,
                 );
                 wordRow = [];
             }
             wordRow.push(
-                <Word
+                <WordButton
                     key={i}
                     onClick={() => {
-                        addWordToMerge(wordList[i]);
+                        addWordToMerge(wordList[i].name);
                     }}
                 >
-                    {wordList[i]}
-                </Word>
+                    {wordList[i].name}
+                </WordButton>,
             );
         }
         result.push(
-            <WordRow key={wordRow.toString()} words={wordRow}></WordRow>
+            <WordRow key={wordRow.toString()} words={wordRow}></WordRow>,
         );
 
         return result;
