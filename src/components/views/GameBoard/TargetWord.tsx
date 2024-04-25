f7ab333bd768cd2fc120b5be2f9d8e01ec56f91d
JacquelineUlken <ulken.jacqueline@gmail.com>
2024-04-25 22:25:41 +0200

    Fixed target word to handle null values

--- src/components/views/GameBoard/TargetWord.tsx
+++ src/components/views/GameBoard/TargetWord.tsx
@@ -5,14 +5,16 @@ import WordButton from "./WordButton";
 import { playerContext } from "./Game";
 
 const TargetWord = () => {
-    const { player} =
+    const { player } =
       useContext(playerContext);
+
+    const targetWordName = player.targetWord ? player.targetWord.name : "";
 
     return (
         <BaseContainer className="targetword container">
             <div className="targetword text">Target word:</div>
             <WordButton key={0} className="targetword word">
-                {player.targetWord}
+                {targetWordName}
             </WordButton>
         </BaseContainer>
     );
