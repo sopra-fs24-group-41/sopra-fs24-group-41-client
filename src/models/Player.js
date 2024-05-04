4ad10c373b56add26dd16eb07e7d04f871ce5f71
JacquelineUlken <ulken.jacqueline@gmail.com>
2024-05-04 17:38:01 +0200

    Adjusted models

--- src/models/Player.js
+++ src/models/Player.js
@@ -6,19 +6,20 @@ import Word from "./Word";
 class Player {
     constructor(data = {}) {
         this.id = null;
-        this.name = null;
+        this.name = "";
         this.points = 0;
         this.token = null;
-        this.wins = null;
-        this.losses = null;
+        this.wins = 0;
+        this.losses = 0;
         this.user = null;
         this.profilePicture = null;
-        this.lobby = null;
+        this.lobbyCode = null;
         this.ownedLobby = null;
-        this.playerWords = null;
+        this.playerWords = [];
         this.targetWord = null;
         this.resultWord = null;
         Object.assign(this, data);
+
         // Assign word count attribute
         this.wordCount = this.playerWords ? this.playerWords.length : 0;
 
@@ -39,19 +40,39 @@ class Player {
 
     getWords() {
         if (!this.playerWords) return [];
-        
+
         return this.playerWords.map(playerWord => new Word(playerWord.word));
     }
-    
+
+    sortPlayerWords () {
+        this.playerWords.sort((a, b) => {
+            // First, compare timestamps
+            if (a.timestamp < b.timestamp) {
+                return -1; // a should come before b
+            } else if (a.timestamp > b.timestamp) {
+                return 1; // b should come before a
+            } else {
+                // If timestamps are equal, compare names alphabetically
+                if (a.word.name < b.word.name) {
+                    return -1; // a should come before b
+                } else if (a.word.name > b.word.name) {
+                    return 1; // b should come before a
+                } else {
+                    return 0; // names are equal (shouldn't happen in this scenario)
+                }
+            }
+        });
+    }
+
     getNewestWord() {
         if (!this.playerWords || this.playerWords.length === 0) {
             return null;
         }
-        
+
         let newestPlayerWord = this.playerWords.reduce((prevWord, currentWord) => {
             const prevTimestamp = new Date(prevWord.timestamp);
             const currentTimestamp = new Date(currentWord.timestamp);
-            
+
             return prevTimestamp > currentTimestamp ? prevWord : currentWord;
         }, this.playerWords[0]);
 
