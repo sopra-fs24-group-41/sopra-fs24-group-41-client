a21cffa378f62315a84687d9efcad70a5d65db61
na50r <shanrosan@gmail.com>
2024-05-15 22:05:16 +0200

    Fixed bug with Result page #91 #109 #114 #120

--- src/components/views/Result.tsx
+++ src/components/views/Result.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import "styles/views/Result.scss";
 import BaseContainer from "components/ui/BaseContainer";
 import IMAGES from "../../assets/images/index1.js";
@@ -6,8 +6,12 @@ import { useNavigate } from "react-router-dom";
 import { api, handleError } from "helpers/api";
 import Player from "../../models/Player.js";
 import { Button } from "components/ui/Button";
+import PropTypes from "prop-types";
+import ICONS from "../../assets/icons/index.js";
+import hashForAnon from "../../helpers/utils";
 
-const Result = () => {
+
+const Result = ({ stompWebSocketHook }) => {
     const navigate = useNavigate();
     const [player, setPlayer] = useState<Player>(new Player);
     const [winner, setWinner] = useState();
@@ -16,7 +20,6 @@ const Result = () => {
     const playerToken = localStorage.getItem("playerToken");
     const lobbyCode = localStorage.getItem("lobbyCode");
     const [selectedPlayer, setSelectedPlayer] = useState<Player | null>(null);
-    const [res, setRes] = useState();
     const resultMessage = { WIN: "You Won!", LOSS: "You Lost..." };
     const [resultStatus, setResultStatus] = useState<boolean>(false);
     const [pID, setPID] = useState();
@@ -29,17 +32,20 @@ const Result = () => {
                 foundPlayer.id = playerId;
                 foundPlayer.token = playerToken;
                 foundPlayer.lobbyCode = lobbyCode;
+                console.log(foundPlayer);
                 setPlayer(foundPlayer);
                 setPID(foundPlayer.id);
             } catch (error) {
                 handleError(error, navigate);
             }
         };
+        
 
         const fetchOtherPlayers = async () => {
             try {
                 let response = await api.get(`/lobbies/${lobbyCode}/players`,);
                 setPlayers(response.data.map(p => new Player(p)));
+                console.log(response.data);
             } catch (error) {
                 handleError(error, navigate);
             }
@@ -66,13 +72,54 @@ const Result = () => {
     const renderIcon = (playerId) => {
         if (winner && playerId === winner.id) return "winner";
         if (playerId === player.id) return "loser";
-        else return "player-icon";
+        else return "player-icon-result";
     };
 
     const handleBackToLobby = () =>{
         let lobbyCode = localStorage.getItem("lobbyCode");
         navigate("/lobby/" + lobbyCode);
     }
+
+    const kick = () => {
+        localStorage.removeItem("playerId");
+        localStorage.removeItem("playerToken");
+        localStorage.removeItem("lobbyCode");
+        navigate("/lobbyoverview");
+    };
+
+    useEffect(() => {
+        if (stompWebSocketHook.connected === true) {
+            stompWebSocketHook.subscribe(`/topic/lobbies/${lobbyCode}`);
+            stompWebSocketHook.subscribe(`/topic/lobbies/${lobbyCode}/game`);
+        }
+
+        return () => {
+            if (stompWebSocketHook.connected === true) {
+                stompWebSocketHook.unsubscribe(`/topic/lobbies/${lobbyCode}`);
+                stompWebSocketHook.unsubscribe(
+                    `/topic/lobbies/${lobbyCode}/game`
+                );
+            }
+            stompWebSocketHook.resetMessagesList();
+        };
+    }, [stompWebSocketHook.connected]);
+
+    useEffect(() => {
+        let messagesLength = stompWebSocketHook.messages.length;
+        if (
+            messagesLength > 0 &&
+            stompWebSocketHook.messages[messagesLength - 1] !== undefined
+        ) {
+            const newObject = stompWebSocketHook.messages[messagesLength - 1];
+            if (newObject.instruction === "start") {
+                navigate("/lobby/game");
+            }
+
+            if (newObject.instruction === "kick") {
+                kick();
+            }
+        }
+    }, [stompWebSocketHook.messages]);
 
     const userContent = (
         <div className="res">
@@ -86,12 +133,12 @@ const Result = () => {
                         onMouseLeave={() => setSelectedPlayer(null)}
                     >
                         <div
-                            className={`player-container ${renderPlayer(
+                            className={`${renderPlayer(
                                 player.id
                             )} ${selectedPlayer === player ? "selected" : ""}`}
                         >
                             <div className={renderIcon(player.id)}>
-                                <img src={IMAGES[player.profilePicture] || IMAGES.BlueFrog} alt="player icon" />
+                                <img src={IMAGES[player.profilePicture] || ICONS[hashForAnon(player.name)]} alt="player icon" />
                             </div>
                             <div className="player-details">
                                 <div className="player-name">
@@ -102,7 +149,7 @@ const Result = () => {
                                     <div className="player-info">
                                         <div>
                                             Words Generated:{" "}
-                                            {player.wordCount || 0}
+                                            {player.wordCount-4}
                                         </div>
                                     </div>
                                 )}
@@ -120,6 +167,18 @@ const Result = () => {
             <Button onClick={handleBackToLobby}>Back to Lobby</Button>
         </BaseContainer>
     );
+};
+
+Result.propTypes = {
+    stompWebSocketHook: PropTypes.shape({
+        subscribe: PropTypes.func.isRequired,
+        unsubscribe: PropTypes.func.isRequired,
+        sendMessage: PropTypes.func.isRequired,
+        messages: PropTypes.array.isRequired,
+        resetMessagesList: PropTypes.func.isRequired,
+        connected: PropTypes.bool.isRequired,
+        subscriptionsRef: PropTypes.object.isRequired,
+    }).isRequired,
 };
 
 export default Result;
