22d8708e30b49559bd6c3fa32dfb659e9ff4058c
Alexandru Hurjui (Aquamarine-12) <hurjuialexandru12@gmail.com>
2024-05-24 19:55:52 +0200

    almost forgot to disable a debugging statement  #142

--- src/components/views/Achievements.tsx
+++ src/components/views/Achievements.tsx
@@ -66,7 +66,7 @@ const Achievements = () => {
         ) {
             return "achievement unlocked";
         } else {
-            return "achievement unlocked";
+            return "achievement locked";
         }
     };
 
